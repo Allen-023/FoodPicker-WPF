da5dc04c98e5549a86c6d51147d4ce377d639577
adam allen <allen.023@hotmail.com>
2021-06-14 14:53:19 -0400

    Bare bones UI. Added button logic. Added user prompts. Added list integration.

--- FoodPicker-WPF/MainWindow.xaml.cs
+++ FoodPicker-WPF/MainWindow.xaml.cs
@@ -20,9 +20,56 @@ namespace FoodPicker_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        protected List<string> foodItemList = new List<string>();
+        
         public MainWindow()
         {
             InitializeComponent();
+
+           
+        }
+
+        // Generate a random int. Use the random number for an index of the food list and display to user.
+        private void PickButton_Click(object sender, RoutedEventArgs e)
+        {
+            Random rndNum = new Random();
+            int randomNumber = rndNum.Next(0,foodItemList.Count());
+
+            OutputTextBox.Text = foodItemList[randomNumber].ToString();     
+        }
+
+        // Adds user input to the food list then prompts the user.
+        private void AddFoodButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (foodItemList.Contains(InputTextBox.Text))
+            {
+                MessageBox.Show("That food item is has been already added. \nTry another food item.");
+            }
+            else
+            {
+                foodItemList.Add(InputTextBox.Text);
+                MessageBox.Show(InputTextBox.Text + " has been added.");
+                InputTextBox.Text = "";
+            }
+
+        }
+
+        // Deletes the user input from the food list if it is present then prompts the user.
+        private void DeleteFoodButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (foodItemList.Contains(InputTextBox.Text))
+            {
+
+                foodItemList.Remove(InputTextBox.Text);
+                MessageBox.Show(InputTextBox.Text + " has been deleted.");
+                InputTextBox.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("That food item is has not been added yet.");
+            }
+
         }
     }
 }
